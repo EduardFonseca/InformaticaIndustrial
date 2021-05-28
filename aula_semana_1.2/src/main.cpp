ef32cb38343dd75389d1ed04c3ad00723b4af072
EduardFonseca <66074930+EduardFonseca@users.noreply.github.com>
2021-05-28 16:09:55 -0300

    teste segundo exercicio

---
+++ aula_semana_1.2/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "mylib.h"
+
+using namespace std;
+
+int main(){
+    cout<<fac(3)<<endl;
+    return 0;
+}
---
+++ aula_semana_1.2/src/mylib.cpp
@@ -0,0 +1,6 @@
+int fac(int num){
+    int result=num;
+    while(num > 0){
+        result*=num--;
+    }
+}
