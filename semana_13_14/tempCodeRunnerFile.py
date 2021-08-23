b3378cffa4fc1240b0377fa2c729ba7bb4724be7
Eduardo Fonseca <fonseca.eduardo@engenharia.ufjf.br>
2021-08-22 22:02:19 -0300

    codigo semana 13 14 finalizado.

--- semana_13_14/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-            'fornalha': 1000,
-            'gas_ref': 1001,
-            'gasolina': 1002,
-            'nafta': 1003,
-            'querosene': 1004,
-            'diesel': 1005,
-            'oleo_lub': 1006,
-            'oleo_comb': 1007,
-            'residuos':
